1f20a8fa66696253d51143bca3f122af43aae4e6
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2023-11-01 13:15:43 +0530

    fix: validation and password vis toggle and icons

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
+import GoogleIcon from "@mui/icons-material/Google";
+import AppleIcon from "@mui/icons-material/Apple";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 export default function Login() {
+	const [formData, setFormData] = useState({ name: "", password: "" });
+	const [showPassword, setShowPassword] = useState(false);
+	const style = {
+		backgroundColor: "#7979794d",
+		height: "1px",
+		border: "none",
+	};
+
 	return (
 		<div className="min-h-screen bg-gradient-to-r from-[#FFEFD7] to-[#FFD7C8] pt-28 pl-28">
 			<div className="flex gap-16">
@@ -11,50 +26,83 @@ export default function Login() {
 					<div className="font-black text-4xl mb-10">
 						Welcome Back
 					</div>
-					<div className="flex gap-2 mb-5 justify-between">
+					<div className="flex gap-2 mb-5 justify-around">
 						<button
 							type="button"
-							className="grow rounded-lg bg-white py-2 px-4"
+							className="rounded-lg bg-white py-2 px-4 flex items-center gap-2 justify-center"
 						>
+							<GoogleIcon />
 							Log in with Google
 						</button>
 						<button
 							type="button"
-							className="grow rounded-lg bg-white py-2 px-4"
+							className="rounded-lg bg-white py-2 px-4 flex items-center gap-2 justify-center"
 						>
+							<AppleIcon />
 							Log in with Apple
 						</button>
 					</div>
 					<div className="flex items-center justify-end">
-						<hr className="w-full inline-block" />
+						<hr className="w-full inline-block" style={style} />
 						<span className=" text-[#797979d4] mx-2">or</span>
-						<hr className="w-full inline-block" />
+						<hr className="w-full inline-block" style={style} />
 					</div>
-					<label htmlFor="name" className="flex flex-col gap-2 mt-8">
-						Name
-						<input
-							type="text"
-							id="name"
-							placeholder="Type Here"
-							autoComplete="name"
-							className="rounded-lg p-2 mb-6"
-						/>
-					</label>
-					<label htmlFor="password" className="flex flex-col gap-2">
-						Password
-						<input
-							type="text"
-							id="password"
-							placeholder="Type Here"
-							className="rounded-lg p-2 mb-12"
-						/>
-					</label>
-					<button
-						type="submit"
-						className="w-full bg-[#0c1f5f] p-2 rounded-lg text-white mb-16"
-					>
-						Log In
-					</button>
+					<form onSubmit={handleSubmit}>
+						<label
+							htmlFor="name"
+							className="flex flex-col gap-2 mt-8"
+						>
+							Name
+							<input
+								type="text"
+								name="name"
+								id="name"
+								value={formData.name}
+								onChange={handleChange}
+								placeholder="Type Here"
+								autoComplete="name"
+								className="rounded-lg p-2 mb-6"
+								required
+							/>
+						</label>
+						<label
+							htmlFor="password"
+							className="flex flex-col gap-2"
+						>
+							Password
+							<div className="relative">
+								<input
+									type={showPassword ? "text" : "password"}
+									name="password"
+									id="password"
+									value={formData.password}
+									onChange={handleChange}
+									minLength={8}
+									placeholder="Type Here"
+									autoComplete="current-password"
+									className="rounded-lg p-2 mb-16 w-full"
+									required
+								/>
+								{!showPassword ? (
+									<VisibilityIcon
+										onClick={handlePassword}
+										className="absolute cursor-pointer right-2 top-2"
+									/>
+								) : (
+									<VisibilityOffIcon
+										onClick={handlePassword}
+										className="absolute cursor-pointer right-2 top-2"
+									/>
+								)}
+							</div>
+						</label>
+						<button
+							type="submit"
+							className="w-full bg-[#0c1f5f] p-2 rounded-lg text-white mb-16"
+						>
+							Log In
+						</button>
+					</form>
 					<Link href="/signup">
 						<span className="text-[#5f6180b8]">
 							First time here?{" "}
@@ -71,4 +119,26 @@ export default function Login() {
 			</div>
 		</div>
 	);
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		try {
+			const res = await axios.get("/v1/login/", formData);
+			console.log(res);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	function handlePassword() {
+		setShowPassword(!showPassword);
+	}
+
+	function handleChange(e) {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	}
 }
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,6 +1,28 @@
+"use client";
+
+import { useState } from "react";
+import axios from "../axios";
 import Link from "next/link";
+import GoogleIcon from "@mui/icons-material/Google";
+import AppleIcon from "@mui/icons-material/Apple";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 export default function Signup() {
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		password: "",
+	});
+
+	const [showPassword, setShowPassword] = useState(false);
+
+	const style = {
+		backgroundColor: "#7979794d",
+		height: "1px",
+		border: "none",
+	};
+
 	return (
 		<div className="min-h-screen bg-gradient-to-r from-[#FFEFD7] to-[#FFD7C8] pt-28 pl-28">
 			<div className="flex gap-16">
@@ -9,60 +31,94 @@ export default function Signup() {
 						Hey there, Welcome
 					</div>
 					<div className="font-black text-4xl mb-10">Get Started</div>
-					<div className="flex gap-2 mb-5 justify-between">
+					<div className="flex gap-2 mb-5 justify-around">
 						<button
 							type="button"
-							className="grow rounded-lg bg-white py-2 px-4"
+							className="rounded-lg bg-white py-2 px-4 flex items-center gap-2 justify-center"
 						>
+							<GoogleIcon />
 							Sign in with Google
 						</button>
 						<button
 							type="button"
-							className="grow rounded-lg bg-white py-2 px-4"
+							className="rounded-lg bg-white py-2 px-4 flex items-center gap-2 justify-center"
 						>
+							<AppleIcon />
 							Sign in with Apple
 						</button>
 					</div>
 					<div className="flex items-center justify-end">
-						<hr className="w-full inline-block" />
+						<hr className="w-full inline-block" style={style} />
 						<span className=" text-[#797979d4] mx-2">or</span>
-						<hr className="w-full inline-block" />
+						<hr className="w-full inline-block" style={style} />
 					</div>
-					<label htmlFor="name" className="flex flex-col gap-1">
-						Name
-						<input
-							type="text"
-							id="name"
-							placeholder="Type Here"
-							autoComplete="name"
-							className="rounded-lg p-2 mb-3"
-						/>
-					</label>
-					<label htmlFor="email" className="flex flex-col gap-1">
-						Email address
-						<input
-							type="text"
-							id="email"
-							placeholder="Type Here"
-							autoComplete="email"
-							className="rounded-lg p-2 mb-3"
-						/>
-					</label>
-					<label htmlFor="password" className="flex flex-col gap-1">
-						Password
-						<input
-							type="text"
-							id="password"
-							placeholder="Type Here"
-							className="rounded-lg p-2 mb-8"
-						/>
-					</label>
-					<button
-						type="submit"
-						className="w-full bg-[#0c1f5f] p-2 rounded-lg text-white mb-16"
-					>
-						Sign Up
-					</button>
+					<form onSubmit={handleSubmit}>
+						<label htmlFor="name" className="flex flex-col gap-1">
+							Name
+							<input
+								type="text"
+								name="name"
+								id="name"
+								value={formData.name}
+								onChange={handleChange}
+								placeholder="Type Here"
+								autoComplete="name"
+								className="rounded-lg p-2 mb-3"
+								required
+							/>
+						</label>
+						<label htmlFor="email" className="flex flex-col gap-1">
+							Email address
+							<input
+								type="email"
+								name="email"
+								id="email"
+								value={formData.email}
+								onChange={handleChange}
+								placeholder="Type Here"
+								autoComplete="email"
+								className="rounded-lg p-2 mb-3"
+								required
+							/>
+						</label>
+						<label
+							htmlFor="password"
+							className="flex flex-col gap-1"
+						>
+							Password
+							<div className="relative">
+								<input
+									type={showPassword ? "text" : "password"}
+									name="password"
+									id="password"
+									value={formData.password}
+									onChange={handleChange}
+									minLength={8}
+									placeholder="Type Here"
+									autoComplete="new-password"
+									className="rounded-lg p-2 mb-8 w-full"
+									required
+								/>
+								{!showPassword ? (
+									<VisibilityIcon
+										onClick={handlePassword}
+										className="absolute cursor-pointer right-2 top-2"
+									/>
+								) : (
+									<VisibilityOffIcon
+										onClick={handlePassword}
+										className="absolute cursor-pointer right-2 top-2"
+									/>
+								)}
+							</div>
+						</label>
+						<button
+							type="submit"
+							className="w-full bg-[#0c1f5f] p-2 rounded-lg text-white mb-16"
+						>
+							Sign Up
+						</button>
+					</form>
 					<Link href="/login">
 						<span className="text-[#5f6180b8]">
 							Have an account?{" "}
@@ -79,4 +135,26 @@ export default function Signup() {
 			</div>
 		</div>
 	);
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		try {
+			const res = await axios.post("/v1/users/", formData);
+			console.log(res);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	function handlePassword(e) {
+		setShowPassword(!showPassword);
+	}
+
+	function handleChange(e) {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	}
 }

863798c69d8f8a3815a62949ac6f1ccfe2991999
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2024-02-01 23:01:16 +0530

    fix: temp data

--- src/components/stories.js
+++ src/components/stories.js
@@ -22,140 +22,144 @@ let stories = [
 	},
 ];
 
-const Characters = [
-		{
-			id: "char1",
-			position: { x: 0, y: 0 },
-			data: { label: "Harry Potter" },
-			type: "input",
-		},
-		{
-			id: "char2",
-			position: { x: 0, y: 100 },
-			data: { label: "Ron Weasley" },
-			type: "input",
-		},
-		{
-			id: "char3",
-			position: { x: 0, y: 200 },
-			data: { label: "Hermione Granger" },
-			type: "input",
-		},
-		{
-			id: "char4",
-			position: { x: 0, y: 300 },
-			data: { label: "Albus Dumbledore" },
-			type: "input",
-		},
-		{
-			id: "char5",
-			position: { x: 0, y: 400 },
-			data: { label: "Severus Snape" },
-			type: "input",
-		},
-		{
-			id: "char6",
-			position: { x: 0, y: 500 },
-			data: { label: "Lord Voldemort" },
-			type: "input",
-		},
-		{
-			id: "char7",
-			position: { x: 0, y: 600 },
-			data: { label: "Ginny Weasley" },
-			type: "input",
-		},
-		{
-			id: "char8",
-			position: { x: 0, y: 700 },
-			data: { label: "Draco Malfoy" },
-			type: "input",
-		},
-		{
-			id: "char9",
-			position: { x: 0, y: 800 },
-			data: { label: "Fred Weasley" },
-			type: "input",
-		},
-		{
-			id: "char10",
-			position: { x: 0, y: 900 },
-			data: { label: "George Weasley" },
-			type: "input",
-		},
-		{
-			id: "char11",
-			position: { x: 0, y: 1000 },
-			data: { label: "Neville Longbottom" },
-			type: "input",
-		},
-	];
+export { stories };
+
+const characters = [
+	{
+		id: "char1",
+		position: { x: 0, y: 0 },
+		data: { label: "Harry Potter" },
+		type: "input",
+	},
+	{
+		id: "char2",
+		position: { x: 0, y: 100 },
+		data: { label: "Ron Weasley" },
+		type: "input",
+	},
+	{
+		id: "char3",
+		position: { x: 0, y: 200 },
+		data: { label: "Hermione Granger" },
+		type: "input",
+	},
+	{
+		id: "char4",
+		position: { x: 0, y: 300 },
+		data: { label: "Albus Dumbledore" },
+		type: "input",
+	},
+	{
+		id: "char5",
+		position: { x: 0, y: 400 },
+		data: { label: "Severus Snape" },
+		type: "input",
+	},
+	{
+		id: "char6",
+		position: { x: 0, y: 500 },
+		data: { label: "Lord Voldemort" },
+		type: "input",
+	},
+	{
+		id: "char7",
+		position: { x: 0, y: 600 },
+		data: { label: "Ginny Weasley" },
+		type: "input",
+	},
+	{
+		id: "char8",
+		position: { x: 0, y: 700 },
+		data: { label: "Draco Malfoy" },
+		type: "input",
+	},
+	{
+		id: "char9",
+		position: { x: 0, y: 800 },
+		data: { label: "Fred Weasley" },
+		type: "input",
+	},
+	{
+		id: "char10",
+		position: { x: 0, y: 900 },
+		data: { label: "George Weasley" },
+		type: "input",
+	},
+	{
+		id: "char11",
+		position: { x: 0, y: 1000 },
+		data: { label: "Neville Longbottom" },
+		type: "input",
+	},
+];
+
+export { characters };
 
 const events = [
 	[
 		{
 			id: "evn-char1-1",
-			position: {x:200,y:0},
+			position: { x: 200, y: 0 },
 			data: { label: "Born" },
 			type: "input",
 		},
 		{
 			id: "evn-char1-2",
-			position: {x:200,y:100},
+			position: { x: 400, y: 0 },
 			data: { label: "Went to Hogwarts" },
 			type: "input",
 		},
 		{
 			id: "evn-char1-3",
-			position: {x:200,y:200},
+			position: { x: 600, y: 0 },
 			data: { label: "Fought Voldemort" },
 			type: "input",
 		},
 		{
 			id: "evn-char1-4",
-			position: {x:200,y:300},
+			position: { x: 800, y: 0 },
 			data: { label: "Married Ginny" },
 			type: "input",
 		},
 		{
 			id: "evn-char1-5",
-			position: {x:200,y:400},
+			position: { x: 1000, y: 0 },
 			data: { label: "Had 3 children" },
 			type: "input",
 		},
 		{
 			id: "evn-char1-6",
-			position: {x:200,y:500},
+			position: { x: 1200, y: 0 },
 			data: { label: "Defeated Voldemort" },
 			type: "input",
 		},
 		{
 			id: "evn-char1-7",
-			position: {x:200,y:600},
+			position: { x: 1400, y: 0 },
 			data: { label: "Died" },
 			type: "input",
 		},
 		{
 			id: "evn-char1-8",
-			position: {x:200,y:700},
+			position: { x: 1600, y: 0 },
 			data: { label: "Became an Auror" },
 			type: "input",
 		},
 		{
 			id: "evn-char1-9",
-			position: {x:200,y:800},
+			position: { x: 1800, y: 0 },
 			data: { label: "Became Headmaster" },
 			type: "input",
 		},
 		{
 			id: "evn-char1-10",
-			position: {x:200,y:900},
+			position: { x: 2000, y: 0 },
 			data: { label: "Became Minister of Magic" },
 			type: "input",
 		},
 		{
 			id: "evn-char1-11",
-			position: {x:200,y:1000},
+			position: { x: 2200, y: 0 },
 			data: { label: "Became a Professor" },
 			type: "input",
 		},
@@ -163,61 +167,61 @@ const events = [
 	[
 		{
 			id: "evn-char2-1",
-			position: {x:200,y:0},
+			position: { x: 200, y: 100 },
 			data: { label: "Born" },
 			type: "input",
 		},
 		{
 			id: "evn-char2-2",
-			position: {x:200,y:100},
+			position: { x: 400, y: 100 },
 			data: { label: "Went to Hogwarts" },
 			type: "input",
 		},
 		{
 			id: "evn-char2-3",
-			position: {x:200,y:200},
+			position: { x: 600, y: 100 },
 			data: { label: "Fought Voldemort" },
 			type: "input",
 		},
 		{
 			id: "evn-char2-4",
-			position: {x:200,y:300},
+			position: { x: 800, y: 100 },
 			data: { label: "Married Hermione" },
 			type: "input",
 		},
 		{
 			id: "evn-char2-5",
-			position: {x:200,y:400},
+			position: { x: 1000, y: 100 },
 			data: { label: "Had 2 children" },
 			type: "input",
 		},
 		{
 			id: "evn-char2-6",
-			position: {x:200,y:500},
+			position: { x: 1200, y: 100 },
 			data: { label: "Defeated Voldemort" },
 			type: "input",
 		},
 		{
 			id: "evn-char2-7",
-			position: {x:200,y:600},
+			position: { x: 1400, y: 100 },
 			data: { label: "Died" },
 			type: "input",
 		},
 		{
 			id: "evn-char2-8",
-			position: {x:200,y:700},
+			position: { x: 1600, y: 100 },
 			data: { label: "Became an Auror" },
 			type: "input",
 		},
 		{
 			id: "evn-char2-9",
-			position: {x:200,y:800},
+			position: { x: 2000, y: 100 },
 			data: { label: "Became Headmaster" },
 			type: "input",
 		},
 		{
 			id: "evn-char2-10",
-			position: {x:200,y:900},
+			position: { x: 2200, y: 100 },
 			data: { label: "Became Minister of Magic" },
 			type: "input",
 		},
@@ -225,67 +229,65 @@ const events = [
 	[
 		{
 			id: "evn-char3-1",
-			position: {x:200,y:0},
+			position: { x: 200, y: 300 },
 			data: { label: "Born" },
 			type: "input",
 		},
 		{
 			id: "evn-char3-2",
-			position: {x:200,y:100},
+			position: { x: 400, y: 300 },
 			data: { label: "Went to Hogwarts" },
 			type: "input",
 		},
 		{
 			id: "evn-char3-3",
-			position: {x:200,y:200},
+			position: { x: 600, y: 300 },
 			data: { label: "Fought Voldemort" },
 			type: "input",
 		},
 		{
 			id: "evn-char3-4",
-			position: {x:200,y:300},
+			position: { x: 800, y: 300 },
 			data: { label: "Married Ron" },
 			type: "input",
 		},
 		{
 			id: "evn-char3-5",
-			position: {x:200,y:400},
+			position: { x: 1000, y: 300 },
 			data: { label: "Had 2 children" },
 			type: "input",
 		},
 		{
 			id: "evn-char3-6",
-			position: {x:200,y:500},
+			position: { x: 1200, y: 300 },
 			data: { label: "Defeated Voldemort" },
 			type: "input",
 		},
 		{
 			id: "evn-char3-7",
-			position: {x:200,y:600},
+			position: { x: 1400, y: 300 },
 			data: { label: "Died" },
 			type: "input",
 		},
 		{
 			id: "evn-char3-8",
-			position: {x:200,y:700},
+			position: { x: 1600, y: 300 },
 			data: { label: "Became an Auror" },
 			type: "input",
 		},
 		{
 			id: "evn-char3-9",
-			position: {x:200,y:800},
+			position: { x: 2000, y: 300 },
 			data: { label: "Became Headmaster" },
 			type: "input",
 		},
 		{
 			id: "evn-char3-10",
-			position: {x:200,y:900},
+			position: { x: 2200, y: 300 },
 			data: { label: "Became Minister of Magic" },
 			type: "input",
 		},
 	],
-]
+];
 
-export default stories;
-
-
+export { events };

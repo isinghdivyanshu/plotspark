295d17d87318a5bcdf842d690c3496c80727f712
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2024-02-02 22:54:11 +0530

    feat: initial flow for timeline

--- src/app/(timeline)/timeline/page.js
+++ src/app/(timeline)/timeline/page.js
@@ -10,51 +10,64 @@ import ReactFlow, {
 	addEdge,
 } from "reactflow";
 import "reactflow/dist/style.css";
+import CustomEdge from "@/components/CustomEdge";
+import CustomNode from "@/components/CustomNode";
 
 export default function Timeline() {
-	let nodes = []
+	const nodeTypes = { customNode: CustomNode };
+	const edgeTypes = { "custom-edge": CustomEdge };
+
+	let nodes = [];
 	characters.forEach((character) => {
-		nodes.push(character)
-	})
+		nodes.push(character);
+	});
 	events.forEach((event) => {
 		event.forEach((e) => {
-			nodes.push(e)
-		})
-	})
-	const initialEdges = [];
-	// { id: "1-2", source: "1", target: "2" }
-	// const [nodes, setNodes] = useState(initialNodes);
-	// const [edges, setEdges] = useState(initialEdges);
+			nodes.push(e);
+		});
+	});
+	// nodes.push({ id: "btn-1", type: "customNode", position: { x: 50, y: 0 } });
 
-	const onNodesChange = useCallback(
-		(changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-		[]
-	);
+	const edges = [
+		{
+			id: "a->b",
+			type: "custom-edge",
+			source: "evn-char1-4",
+			target: "evn-char1-5",
+		},
+		{
+			id: "b->c",
+			source: "evn-char1-6",
+			target: "evn-char1-7",
+		},
+	];
 
-	const onEdgesChange = useCallback(
-		(changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-		[]
-	);
-
-	const onConnect = useCallback(
-		(params) => setEdges((eds) => addEdge(params, eds)),
-		[]
-	);
+	// const onNodesChange = useCallback(
+	// 	(changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+	// 	[]
+	// );
+	// const onEdgesChange = useCallback(
+	// 	(changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+	// 	[]
+	// );
+	// const onConnect = useCallback(
+	// 	(params) => setEdges((eds) => addEdge(params, eds)),
+	// 	[]
+	// );
 
 	return (
 		<div className="h-full bg-white dark:bg-[#1a1d28]">
 			<div className="h-full">
 				<ReactFlow
 					nodes={nodes}
-					// edges={edges}
+					edges={edges}
+					nodesConnectable={false}
 					// onEdgesChange={onEdgesChange}
-					onConnect={onConnect}
-					
+					// onConnect={onConnect}
 					panOnScroll
 					// TODO: implement drag and drop for nodes
 					// snapToGrid={true}
 					// snapGrid={[100,100]}
-
 				>
 					<Background />
 					<Controls />

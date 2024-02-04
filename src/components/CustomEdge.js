0d5bc243e590813ceef40516fd234ae0758313ce
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2024-02-04 16:31:26 +0530

    feat: add charNode inputNode outputNode customEdge

--- src/components/CustomEdge.js
+++ src/components/CustomEdge.js
@@ -1,19 +1,14 @@
-import {
-	BaseEdge,
-	EdgeLabelRenderer,
-	getStraightPath,
-	useReactFlow,
-} from "reactflow";
+import { BaseEdge, EdgeLabelRenderer, getStraightPath } from "reactflow";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 export default function CustomEdge({ id, sourceX, sourceY, targetX, targetY }) {
-	const { setEdges } = useReactFlow();
 	const [edgePath, labelX, labelY] = getStraightPath({
 		sourceX,
 		sourceY,
 		targetX,
 		targetY,
 	});
-
+	// console.log(id);
 	return (
 		<>
 			<BaseEdge id={id} path={edgePath} />
@@ -24,12 +19,9 @@ export default function CustomEdge({ id, sourceX, sourceY, targetX, targetY }) {
 						transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
 						pointerEvents: "all",
 					}}
-					className="nodrag nopan"
-					onClick={() => {
-						setEdges((es) => es.filter((e) => e.id !== id));
-					}}
+					className="nodrag rounded-full bg-transparent"
 				>
-					delete
+					<AddCircleOutlineIcon className="text-sm hover:scale-125 bg-white rounded-full text-center" />
 				</button>
 			</EdgeLabelRenderer>
 		</>
--- src/components/CustomNode.js
+++ src/components/CustomNode.js
@@ -1,16 +1,73 @@
+import React, { memo } from "react";
 import { Handle, Position } from "reactflow";
 
-const CustomNode = ({ data }) => {
+function CharNode({ data }) {
+	const nodeColors = ["#D31D8A", "#B1F1BC", "#DEE4F7", "#FFE298", "#4467DE"];
+	// nodeColors[index % nodeColors.length];
 	return (
-		<>
-			<button className="rounded-full" style={{ padding: "10px 20px" }}>
-				Divyanshu
-			</button>
-
-			<Handle type="target" position={Position.Left} />
-			<Handle type="source" position={Position.Right} />
-		</>
+		<button
+			className={`nodrag bg-[#DEE4F7] px-4 py-2 shadow-md rounded-md !border-[${
+				nodeColors[data.index % nodeColors.length]
+			}] text-lg font-bold hover:scale-110`}
+		>
+			{data.name}
+		</button>
 	);
-};
+}
+export const CharacterNode = memo(CharNode);
 
-export default CustomNode;
+function DefNode({ data }) {
+	return (
+		<button
+			className={`nodrag px-4 py-2 shadow-md rounded-md bg-white border-2 !border-[${data.color}] text-lg font-bold hover:scale-110`}
+			onClick={() => {
+				console.log("Clicked");
+			}}
+		>
+			{data.title}
+			<Handle
+				type="target"
+				position={Position.Left}
+				className="!bg-teal-500"
+			/>
+			<Handle
+				type="source"
+				position={Position.Right}
+				className="!bg-teal-500"
+			/>
+		</button>
+	);
+}
+export const DefaultNode = memo(DefNode);
+
+function InNode({ data }) {
+	return (
+		<button
+			className={`nodrag px-4 py-2 shadow-md rounded-md bg-white border-2 !border-[${data.color}] text-lg font-bold hover:scale-110`}
+		>
+			{data.title}
+			<Handle
+				type="source"
+				position={Position.Right}
+				className=" !bg-teal-500"
+			/>
+		</button>
+	);
+}
+export const InputNode = memo(InNode);
+
+function OutNode({ data }) {
+	return (
+		<button
+			className={`nodrag px-4 py-2 shadow-md rounded-md bg-white border-2 !border-[${data.color}] text-lg font-bold hover:scale-110`}
+		>
+			{data.title}
+			<Handle
+				type="target"
+				position={Position.Left}
+				className=" !bg-teal-500"
+			/>
+		</button>
+	);
+}
+export const OutputNode = memo(OutNode);

164c2d936c38ac4257eb0c3a165c94d6bc12ca47
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2024-02-05 23:14:13 +0530

    chore: modified backend baseURL

--- src/app/axios.js
+++ src/app/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "http://localhost:4000" });
+const instance = axios.create({
+	baseURL: "https://ff-writer-chowswebsite.koyeb.app/",
+});
 
 export default instance;

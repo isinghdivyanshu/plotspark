1d48161fbec77122ce4d7626ab17c70476aa8ee1
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2024-03-05 00:24:34 +0530

    fix:auto logout function

--- src/app/providers/AuthProvider.js
+++ src/app/providers/AuthProvider.js
@@ -3,18 +3,26 @@
 import { useEffect } from "react";
 import { useStore } from "@/app/store";
 import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
 export default function AuthProvider({ children }) {
 	const router = useRouter();
 	const { logout, initializeFromLocalStorage } = useStore();
 
+	function logoutTimer(tokenExpiry) {
+		setTimeout(() => {
+			logout();
+			router.push("/login");
+			toast.info("Session expired.");
+		}, tokenExpiry.getTime() - new Date().getTime());
+	}
+
 	useEffect(() => {
 		const expiry = localStorage.getItem("expiry");
 		const tokenExpiry = new Date(expiry);
-		const logoutTimer = setTimeout(() => {
-			logout();
-			router.replace("/login");
-		}, tokenExpiry.getTime() - new Date().getTime());
+		if (expiry) {
+			logoutTimer(tokenExpiry);
+		}
 
 		return () => clearTimeout(logoutTimer);
 	}, []);
